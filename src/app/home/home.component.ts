b216ea81834496249e392c42b4b8d83073163c66
Saket Verma <saket3324verma@gmail.com>
2024-02-23 22:20:24 +0530

    side bar close / open

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  isSideOpen : boolean = true;
+
+  openSideBar() {
+    this.isSideOpen = !this.isSideOpen;
+  }
+
 }
